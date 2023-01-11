a8304134638d4ad2ac81a520f7002660a40c0d46
Jillien <drmythology@gmail.com>
2023-01-12 02:39:34 +0800

    modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
-document.addEventListener("click", function (){
-    if (event.target.matches("button")){
-        alert("I got clicked!");
+// document.addEventListener("click", function (){
+//     if (event.target.matches("button")){
+//         // alert("I got clicked!");
+//         var tom1 = new Audio('sounds/tom-1.mp3');
+//         tom1.play();
+//     }
+// })
+
+// Waiting for button press......
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    }); 
+}
+// Waiting for keyboard press......
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+// Main function to produce audio
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        default:
+            break;
     }
-})
+};
+
+// Function to highlight the pressed state
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
